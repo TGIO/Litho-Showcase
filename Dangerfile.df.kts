8048673f1055e848bc477a9f3419c138d506bc03
Giorgi TGIO Tabatadze <gio.caporegime@gmail.com>
2021-02-18 16:30:10 +0400

    Update Dangerfile.df.kts

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,13 +1,15 @@
 @file:DependsOn("com.gianluz:danger-kotlin-android-lint-plugin:0.1.0")
 
 import com.gianluz.dangerkotlin.androidlint.AndroidLint
-import com.gianluz.dangerkotlin.androidlint.androidLint
 import systems.danger.kotlin.*
 
 
 fun checkDetekt(danger: DangerDSL) {
     danger.git.modifiedFiles.forEach {
         message("Modified file", it, 0)
+    }
+    danger.git.createdFiles.forEach {
+        message("Created file", it, 0)
     }
 }
 
@@ -17,27 +19,11 @@ danger(args) {
     val danger = this
 
     onGitHub {
-        val github = this
+        // val github = this
         checkDetekt(danger)
         message("onGithub done")
 
-        androidLint {
-            // Fail for each Fatal in a single module
-            val moduleLintFilePaths = find(
-                "app",
-                "lint-results-debug.xml",
-                "lint-results-release.xml"
-            ).toTypedArray()
-
-            parseAllDistinct(*moduleLintFilePaths).forEach {
-                if(it.severity == "Fatal")
-                    fail(
-                        "Danger lint check failed: ${it.message}",
-                        it.location.file.replace(System.getProperty("user.dir"), ""),
-                        Integer.parseInt(it.location.line)
-                    )
-            }
-            message("androidLint done")
-        }
+        AndroidLint.report("androidlint.xml")
+        message("androidLint done")
     }
 }
