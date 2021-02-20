7ce4dcb9cbd150a0f94d3ed26c0c7220c75352e1
Giorgi Tabatadze <gio.caporegime@gmail.com>
2021-02-20 12:30:45 +0400

    dngr

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,21 +1,44 @@
+@file:Repository("https://jcenter.bintray.com/")
+@file:DependsOn("com.beust:klaxon:5.0.1")
+
+import com.beust.klaxon.Klaxon
 import systems.danger.kotlin.*
 
 val prefix = """$""""
 val sufix = """""""
-val ktlintCMD = "${prefix}ktlint -a --color --reporter=json?group_by_file"
+val rawCmnd = "ktlint -a --color --reporter=json"
+val ktlintCMD = "${prefix}$rawCmnd"
+
+class Error(
+    val line: Int,
+    val column: Int,
+    val message: String,
+    val rules: String
+)
 
 fun checkDetekt(danger: DangerDSL, git: Git) {
-    git.modifiedFiles.forEach {
-        // val cmndToRun = """${ktlintCMD} $it$sufix"""
-        // val cmnd = danger.utils.exec(cmndToRun, emptyList())
+    git.modifiedFiles.forEach { filePath ->
+        val cmndToRun = """$ktlintCMD $filePath$sufix"""
+        // val cmndToRun = rawCmnd
+        val result = danger.utils.exec(cmndToRun, emptyList())
 
-        val cmndToRun = """ktlint "$it""""
-        val cmnd = danger.utils.exec(cmndToRun)
+        // val cmndToRun = """ktlint "$it""""
+        // val cmnd = danger.utils.exec(cmndToRun)
+        println("run: $rawCmnd $filePath")
 
-        if (cmnd.isNullOrEmpty()) {
-            message("Modified file - ${it}")
+        if (result.isNullOrEmpty()) {
+            message("Modified file - $filePath")
         } else {
-            warn("Modified file - ${it}; $cmnd", it, 0)
+            Klaxon()
+                .parse<List<Error>>(result)
+                ?.let { findings ->
+                    findings.forEach {
+                        warn(it.rules, filePath, it.line)
+                    }
+                } ?: run {
+                        warn("Modified file - $filePath; $result", filePath, 0)
+                     }
+
             message("Command $cmndToRun")
         }
     }
@@ -33,6 +56,14 @@ danger(args) {
 
     onGit {
         checkDetekt(danger, this)
-        message("onGit done modifiedFiles: ${this.modifiedFiles.size}, createdFiles: ${this.createdFiles.size}, deletedFiles: ${this.deletedFiles.size}")
+        message(
+            "onGit done modifiedFiles: ${
+                this.modifiedFiles.size
+            }, createdFiles: ${
+                this.createdFiles.size
+            }, deletedFiles: ${
+                this.deletedFiles.size
+            }"
+        )
     }
 }
